ad96e98a105d0ae3c0fc0b1eab18698cd18ac8aa
benoitD03 <bens.92@hotmail.fr>
2024-09-07 15:40:02 +0200

    Add search method with google books api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {SearchComponent} from "./pages/search/search.component";
 export const routes: Routes = [
   {path: '', component: DashboardComponent},
   {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'search', component: SearchComponent},
 
 ];
