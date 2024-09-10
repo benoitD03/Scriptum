fa73189563212121694b28f1db05e6ed212efe07
benoitD03 <bens.92@hotmail.fr>
2024-09-10 16:45:47 +0200

    🔵 Edit menu + 🟡 Creating routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 import {SearchComponent} from "./pages/search/search.component";
+import {LibraryComponent} from "./pages/library/library.component";
+import {ProfileComponent} from "./pages/profile/profile.component";
 export const routes: Routes = [
   {path: '', component: DashboardComponent},
   {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'library', component: LibraryComponent},
   {path: 'search', component: SearchComponent},
+  {path: 'profile', component: ProfileComponent},
 
 ];
