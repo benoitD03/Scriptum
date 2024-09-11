e9a4a0ecfca51f0379bf626344b38617aa38d85b
benoitD03 <bens.92@hotmail.fr>
2024-09-11 16:33:14 +0200

    🟡 Adding book to library functionnality

--- src/app/components/book-searched/book-searched.component.ts
+++ src/app/components/book-searched/book-searched.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {NgIf} from "@angular/common";
+import {Book} from "../../class/book";
+import {SupabaseService} from "../../services/supabase.service";
 
 @Component({
   selector: 'app-book-searched',
@@ -13,10 +15,28 @@ import {NgIf} from "@angular/common";
 export class BookSearchedComponent {
   @Input() book: any;
 
+  constructor(private supabaseService: SupabaseService) {
+  }
+
   /**
    * Méthode pour fermer le modal
    */
   closeModal() {
     this.book = null;
+  }
+
+  /**
+   * Méthode pour ajouter un livre à la bibliothèque
+   * La partie user est gérée dans le service SupabaseService
+   */
+  async createBook() {
+    const newBook: Book = {
+      title: this.book.volumeInfo.title,
+      author: this.book.volumeInfo.authors[0],
+      image: this.book.volumeInfo.imageLinks.thumbnail,
+      description: this.book.volumeInfo.description,
+      finished: false
+    };
+    await this.supabaseService.createBook(newBook);
   }
 }
