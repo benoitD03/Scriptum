f356b036ab876ae8defbcb5a53d8a6301eea56e1
benoitD03 <bens.92@hotmail.fr>
2024-10-10 11:29:01 +0200

    🟡 Adding update note method

--- src/app/pages/note-form/note-form.component.ts
+++ src/app/pages/note-form/note-form.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NgIf} from "@angular/common";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Location} from "@angular/common";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SupabaseService} from "../../services/supabase.service";
@@ -20,10 +20,17 @@ import {Note} from "../../class/note";
 export class NoteFormComponent implements OnInit {
   book_id: number = 0;
   note: Note = new Note();
-  constructor(private route : ActivatedRoute, private location: Location, private supabaseService: SupabaseService) { }
+  state: any;
+  constructor(private router: Router, private route : ActivatedRoute, private location: Location, private supabaseService: SupabaseService) { }
 
   ngOnInit() {
     this.book_id = Number(this.route.snapshot.paramMap.get('book_id'));
+    this.state = history.state;
+    if (this.state && this.state.note) {
+      const note = this.state.note as Note;
+      this.note.chapter = note.chapter;
+      this.note.text = note.text;
+    }
   }
 
   goBack(): void {
@@ -40,11 +47,25 @@ export class NoteFormComponent implements OnInit {
     };
     this.supabaseService.createNote(newNote, this.book_id).subscribe({
       next: (note) => {
-        console.log('Note créée:', note);
         this.goBack()
       },
       error: (error) => {
         console.error('Erreur lors de la création de la note:', error.message);
+      }
+    });
+  }
+
+  updateNote() {
+    const updatedNote: Note = {
+      text: this.note.text,
+      chapter: this.note.chapter
+    };
+    this.supabaseService.updateNoteById(this.state.note.id, updatedNote).subscribe({
+      next: (note) => {
+        this.goBack();
+      },
+      error: (error) => {
+        console.error('Erreur lors de la modification de la note:', error.message);
       }
     });
   }
